1b858d4e0e5dd6a1592a392f54c095869d72da94
SkutinAnton <skutin_anton@mail.ru>
2025-02-13 20:53:57 +0300

    init

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,36 @@
 #!/usr/bin/env node
 
-console.log("Hello from my CLI tool!");
+import fs from 'fs-extra';
+import path from 'path';
+import chalk from 'chalk';
+import { Command } from 'commander';
 
-// Пример обработки аргументов командной строки
-const args = process.argv.slice(2);
-if (args.length > 0) {
-  console.log("Arguments:", args);
-} else {
-  console.log("No arguments provided.");
-}
+const program = new Command();
+
+program
+  .name('started-template-cli')
+  .description('Инициализация шаблона проекта')
+  .version('1.0.0');
+
+program
+  .command('<mode>')
+  .description('Создание проекта...')
+  .action((mode) => {
+    console.log("mode", mode);
+    if (mode && ["standalone", "export"].includes(mode)) {
+      const source = path.join(__dirname, `../templates/${mode}`);
+      const target = ".";
+
+      console.log("source", source);
+
+
+      fs.copySync(source, target);
+      console.log(chalk.green("Создание проекта завершено"));
+    } else {
+      console.log(chalk.red("Не выбран режим. Передайте аргумент 'standalone' или 'export'"));
+      process.exit(1);
+    }
+  });
+
+// Парсинг аргументов командной строки
+program.parse(process.argv);
---
+++ templates/export/components/main/Main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Main = () => {
+    return (
+            <p>EXPORT</p>
+    );
+};
+
+export default Main;
