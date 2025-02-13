53d5f1fd5f3a6bbec8887a6ec09df0cc5fa700bd
SkutinAnton <skutin_anton@mail.ru>
2025-02-13 22:34:17 +0300

    init

--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,6 @@ const fs = require('fs-extra');
 const path = require('path');
 const chalk = require('chalk');
 const inquirer = require('inquirer');
-
 const { Command } = require('commander');
 
 const program = new Command();
