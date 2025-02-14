1f0e609d49b38f3a57aabb7ce639b4c7e716146b
SkutinAnton <skutin_anton@mail.ru>
2025-02-14 18:43:41 +0300

    init

--- bin/cli.js
+++ bin/cli.js
@@ -1,34 +1,57 @@
 #!/usr/bin/env node
 
-const fs = require('fs-extra');
-const path = require('path');
-const chalk = require('chalk');
-const inquirer = require('inquirer');
-const { Command } = require('commander');
+const fs = require("fs-extra");
+const path = require("path");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const { Command } = require("commander");
 
 const program = new Command();
 
+const targetDir = ".";
 const questions = [
   {
-    type: 'list',
-    name: 'mode',
-    message: 'Выберите режим:',
-    choices: ['export (SPA)', 'standalone (SSR)'],
+    type: "list",
+    name: "mode",
+    message: "Выберите режим:",
+    choices: ["export (SPA)", "standalone (SSR)"],
   },
 ];
 
-program
-  .name('started-template-cli')
-  .action(async () => {
-    console.log(chalk.blue.bold('Инициализация нового проекта'));
+const deleteAllFiles = async () => {
+  try {
+    const files = await fs.readdir(targetDir);
 
-    const { mode } = await inquirer.prompt(questions);
-    const template = mode.split(" ")[0];
-    const source = path.join(__dirname, `../templates/${template}`);
-    const target = '.';
+    if (files.length) {
+      files.forEach((item) => {
+        const itemPath = path.join(targetDir, item);
 
-    fs.copySync(source, target);
-    console.log(chalk.green.bold('Создание проекта завершено'));
-  });
+        if (fs.statSync(itemPath).isDirectory()) {
+          fs.rmSync(itemPath, { recursive: true });
+        } else {
+          fs.unlinkSync(itemPath);
+        }
+      });
+    }
+  } catch (err) {
+    console.log(chalk.red.bold(`Ошибка при удалении файлов: ${err}`));
+    process.exit(1);
+  }
+};
+
+const createTemplate = async () => {
+  console.log(chalk.blue.bold("Инициализация нового проекта"));
+
+  const { mode } = await inquirer.prompt(questions);
+  const templateName = mode.split(" ")[0];
+  const templateDir = path.join(__dirname, `../templates/${templateName}`);
+
+  await deleteAllFiles();
+
+  fs.copySync(templateDir, targetDir);
+  console.log(chalk.green.bold("Создание проекта завершено"));
+};
+
+program.name("started-template-cli").action(createTemplate);
 
 program.parse(process.argv);
